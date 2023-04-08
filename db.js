8efc4d006893401a9cc47c35e22c6bf142c26c78
Jitendra Kumar <jitendrakumarbunkar85@gmail.com>
2023-04-08 23:09:12 +0530

    final commit

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const mongoose=require("mongoose");
-const mongoURI=process.env.REACT_APP_MONGO_URI
-// const mongoURI="mongodb+srv://root:root022@cluster0.rq55vdi.mongodb.net/inotebook"
-const connectToMongo=()=>{
+const mongoURI=`${process.env.REACT_APP_MONGO_URI}`;
+const connectToMongo= ()=>{
     try {
-        mongoose.set('strictQuery', false);
-        mongoose.connect(mongoURI, { useNewUrlParser: true },()=>{
+         mongoose.set('strictQuery', false);
+         mongoose.connect(mongoURI,()=>{
             console.log("Connected To Mongo Successfully")})
-        
     } catch (error) {
         console.log(error)
     }
